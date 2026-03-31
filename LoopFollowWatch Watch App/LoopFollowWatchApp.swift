dd5d88bb3ef389f01724805c4d530e169ee3cbeb
Phil A <76601115+MtlPhil@users.noreply.github.com>
2026-03-31 18:58:07 -0400

    Update LoopFollowWatchApp.swift

--- LoopFollowWatch Watch App/LoopFollowWatchApp.swift
+++ LoopFollowWatch Watch App/LoopFollowWatchApp.swift
@@ -1,11 +1,13 @@
 // LoopFollowWatchApp.swift
-// Philippe Achkar
 // 2026-03-10
 
 import SwiftUI
+import WatchKit
 
 @main
 struct LoopFollowWatch_Watch_AppApp: App {
+
+    @WKApplicationDelegateAdaptor(WatchAppDelegate.self) var delegate
 
     init() {
         WatchSessionReceiver.shared.activate()
@@ -14,6 +16,27 @@ struct LoopFollowWatch_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+// MARK: - App Delegate
+
+final class WatchAppDelegate: NSObject, WKApplicationDelegate {
+
+    func applicationDidFinishLaunching() {
+        WatchSessionReceiver.shared.activate()
+    }
+
+    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+        for task in backgroundTasks {
+            switch task {
+            case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
+                WatchSessionReceiver.shared.beginHandling(task: connectivityTask)
+
+            default:
+                task.setTaskCompletedWithSnapshot(false)
+            }
         }
     }
 }
